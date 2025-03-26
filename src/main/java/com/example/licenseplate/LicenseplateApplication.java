89249d5859c36808312bc1e9b33f469307362433
minhfat69 <minhnl0588@ut.edu.vn>
2025-03-26 23:50:56 +0700

    them vao

---
+++ src/main/java/com/example/licenseplate/LicenseplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.licenseplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseplateApplication.class, args);
+	}
+
+}
