f504c8fcb356135b1a6b6ca89d72dae3d37d8771
Tuấn Khang <nguyenhuutuankhang412@gmail.com>
2025-05-27 21:10:57 +0700

    feat: release final version 2.0.0

--- src/main/java/com/example/licenseplate/repository/MotorcycleViolationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.licenseplate.repository;
-
-import com.example.licenseplate.model.MotorcycleViolation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MotorcycleViolationRepository extends JpaRepository<MotorcycleViolation, Integer> {
-}
